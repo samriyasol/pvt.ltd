bb96aa463417198b9b86e638b902d13190afb513
samriyasol <khabrenlive@gmail.com>
2026-03-26 20:37:22 +0530

    Update page.js to import and use StatisticBorder component around the hero heading and description.

---
+++ page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StatisticBorder from './StatisticBorder';
+
+const Page = () => {
+    return (
+        <StatisticBorder>
+            <h1>Hero Heading</h1>
+            <p>Description goes here.</p>
+        </StatisticBorder>
+    );
+};
+
+export default Page;
